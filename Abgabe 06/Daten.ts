f9c2aae6f629783bce6ce0e55667488481f2ea49
TimSchneider1 <timleander03@gmail.com>
2022-11-20 17:48:02 +0100

    zwischenstand

---
+++ Abgabe 06/Daten.ts
@@ -0,0 +1,13 @@
+let italien:string = "Italien";
+let alterItalien:number = 47.6;
+
+let frankreich:string = "Frankreich";
+let alterFrankreich:number = 42.1;
+
+let deutschland:string = "Deutschland";
+let alterDeutschland:number = 45.9;
+
+let rumänien:string = "Rumänien";
+let alterRumänien:number = 43.0;
+
+console.log("Das durchschnittliche Alter in "+italien+"beträgt"+alterItalien);
---
+++ Abgabe 06/daten.js
@@ -0,0 +1,10 @@
+let italien = "Italien";
+let alterItalien = 47.6;
+let frankreich = "Frankreich";
+let alterFrankreich = 42.1;
+let deutschland = "Deutschland";
+let alterDeutschland = 45.9;
+let rumänien = "Rumänien";
+let alterRumänien = 43.0;
+console.log("Das durchschnittliche Alter in " + italien + "beträgt" + alterItalien);
+//# sourceMappingURL=daten.js.map
