bc0052ef2bd68374819e681a1c656d6bb728378e
TimSchneider1 <timleander03@gmail.com>
2022-11-24 14:04:37 +0100

    Aufgabe7

---
+++ Abgabe07/daten.js
@@ -0,0 +1,17 @@
+let italien = "Italien";
+let alterItalien = 47.6;
+let frankreich = "Frankreich";
+let alterFrankreich = 42.1;
+let deutschland = "Deutschland";
+let alterDeutschland = 45.9;
+let rumänien = "Rumänien";
+let alterRumänien = 43.0;
+let summe = alterItalien + alterFrankreich + alterDeutschland + alterRumänien;
+let durchschnitt = summe / 4;
+console.log("Das durchschnittliche Alter in " + italien + " beträgt " + alterItalien + " Jahre");
+console.log("Das durchschnittliche Alter in " + frankreich + " beträgt " + alterFrankreich + " Jahre");
+console.log("Das durchschnittliche Alter in " + deutschland + " beträgt " + alterDeutschland + " Jahre");
+console.log("Das durchschnittliche Alter in " + rumänien + " beträgt " + alterRumänien + " Jahre");
+console.log("Das durchschnittliche Alter aller aufgeführten Länder beträgt " + durchschnitt + " Jahre");
+// Ich habe nur eine kleine Berechnung durchgeführt, da ich nicht genügend Daten für das durchschnittliche Alter aus vergangenen Jahren finden konnte
+//# sourceMappingURL=daten.js.map
---
+++ Abgabe07/daten.ts
@@ -0,0 +1,23 @@
+let italien:string = "Italien";
+let alterItalien:number = 47.6;
+
+let frankreich:string = "Frankreich";
+let alterFrankreich:number = 42.1;
+
+let deutschland:string = "Deutschland";
+let alterDeutschland:number = 45.9;
+
+let rumänien:string = "Rumänien";
+let alterRumänien:number = 43.0;
+
+let summe:number = alterItalien+alterFrankreich+alterDeutschland+alterRumänien;
+let durchschnitt:number = summe/4;
+
+console.log("Das durchschnittliche Alter in "+italien+" beträgt "+alterItalien+" Jahre");
+console.log("Das durchschnittliche Alter in "+frankreich+" beträgt "+alterFrankreich+" Jahre");
+console.log("Das durchschnittliche Alter in "+deutschland+" beträgt "+alterDeutschland+" Jahre");
+console.log("Das durchschnittliche Alter in "+rumänien+" beträgt "+alterRumänien+" Jahre");
+
+console.log("Das durchschnittliche Alter aller aufgeführten Länder beträgt "+durchschnitt+" Jahre");
+
+// Ich habe nur eine kleine Berechnung durchgeführt, da ich nicht genügend Daten für das durchschnittliche Alter aus vergangenen Jahren finden konnte
