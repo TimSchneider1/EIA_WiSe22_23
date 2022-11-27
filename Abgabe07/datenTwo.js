91057f72c36ed2536e77408a5d9de87bbe296412
TimSchneider1 <timleander03@gmail.com>
2022-11-27 18:08:38 +0100

    zwischenstand

---
+++ Abgabe07/datenTwo.js
@@ -0,0 +1,43 @@
+var A07;
+(function (A07) {
+    let italien = "Italien";
+    let alterItalien = 47.6;
+    let frankreich = "Frankreich";
+    let alterFrankreich = 42.1;
+    let deutschland = "Deutschland";
+    let alterDeutschland = 45.9;
+    let rumänien = "Rumänien";
+    let alterRumänien = 43.0;
+    let summe = alterItalien + alterFrankreich + alterDeutschland + alterRumänien;
+    let durchschnitt = summe / 4;
+    function consoleOutput(countryName, age) {
+        console.log("Das durchschnittliche Alter in " + countryName + " beträgt " + age + " Jahre.");
+    }
+    ;
+    consoleOutput(italien, alterItalien);
+    consoleOutput(frankreich, alterFrankreich);
+    consoleOutput(deutschland, alterDeutschland);
+    consoleOutput(rumänien, alterRumänien);
+    const buttonGer = document.getElementById("buttonDE");
+    buttonGer.addEventListener("click", function () {
+        document.getElementById("text").innerText = "Durchschnittliches Alter in Deutschland: " + alterDeutschland + " Jahre";
+    });
+    const buttonFra = document.getElementById("buttonF");
+    buttonFra.addEventListener("click", function () {
+        document.getElementById("text").innerText = "Durchschnittliches Alter in Frankreich: " + alterFrankreich + " Jahre";
+    });
+    const buttonIta = document.getElementById("buttonI");
+    buttonIta.addEventListener("click", function () {
+        document.getElementById("text").innerText = "Durchschnittliches Alter in Italien: " + alterItalien + " Jahre";
+    });
+    const buttonRu = document.getElementById("buttonR");
+    buttonRu.addEventListener("click", function () {
+        document.getElementById("text").innerText = "Durchschnittliches Alter in Rumänien: " + alterRumänien + " Jahre";
+        document.querySelector(".chartWrapper").setAttribute('style', 'height:' + alterRumänien);
+    });
+    const buttonSt = document.getElementById("Stern");
+    buttonSt.addEventListener("click", function () {
+        document.getElementById("text").innerText = "Durchschnittliches Alter in allen aufgeführten Ländern: " + durchschnitt + " Jahre";
+    });
+})(A07 || (A07 = {}));
+//# sourceMappingURL=datenTwo.js.map
---
+++ Abgabe07/datenTwo.ts
@@ -0,0 +1,62 @@
+namespace A07{
+
+    let italien:string = "Italien";
+    let alterItalien:number = 47.6;
+
+    let frankreich:string = "Frankreich";
+    let alterFrankreich:number = 42.1;
+
+    let deutschland:string = "Deutschland";
+    let alterDeutschland:number = 45.9;
+
+    let rumänien:string = "Rumänien";
+    let alterRumänien:number = 43.0;
+
+    let summe:number = alterItalien+alterFrankreich+alterDeutschland+alterRumänien;
+    let durchschnitt:number = summe/4;
+
+    function consoleOutput (countryName: string, age: number): void {
+        console.log ("Das durchschnittliche Alter in "+countryName+" beträgt "+age+" Jahre." );
+    };
+
+    consoleOutput(italien, alterItalien);
+    consoleOutput(frankreich, alterFrankreich);
+    consoleOutput(deutschland, alterDeutschland);
+    consoleOutput(rumänien, alterRumänien);
+
+    
+    const buttonGer: HTMLButtonElement = document.getElementById("buttonDE") as HTMLButtonElement;
+
+    buttonGer.addEventListener("click", function(){
+        document.getElementById("text").innerText = "Durchschnittliches Alter in Deutschland: "+alterDeutschland+" Jahre";
+    });
+
+
+    const buttonFra: HTMLButtonElement = document.getElementById("buttonF") as HTMLButtonElement;
+
+    buttonFra.addEventListener("click", function(){
+        document.getElementById("text").innerText = "Durchschnittliches Alter in Frankreich: "+alterFrankreich+" Jahre";
+    });
+
+
+    const buttonIta: HTMLButtonElement = document.getElementById("buttonI") as HTMLButtonElement;
+
+    buttonIta.addEventListener("click", function(){
+        document.getElementById("text").innerText = "Durchschnittliches Alter in Italien: "+alterItalien+" Jahre";
+    });
+
+    const buttonRu: HTMLButtonElement = document.getElementById("buttonR") as HTMLButtonElement;
+
+    buttonRu.addEventListener("click", function(){
+        document.getElementById("text").innerText = "Durchschnittliches Alter in Rumänien: "+alterRumänien+" Jahre";
+        document.querySelector(".chartWrapper").setAttribute('style', 'height:'+alterRumänien)
+    });
+
+
+    const buttonSt: HTMLButtonElement = document.getElementById("Stern") as HTMLButtonElement;
+
+    buttonSt.addEventListener("click", function(){
+        document.getElementById("text").innerText = "Durchschnittliches Alter in allen aufgeführten Ländern: "+durchschnitt+" Jahre";
+    });
+    
+}
