2287a491041318f5cbb50e9e8b7c9a0614506d6a
TimSchneider1 <timleander03@gmail.com>
2022-12-03 14:09:55 +0100

    final

--- Abgabe08/drums.js
+++ Abgabe08/drums.js
@@ -11,9 +11,54 @@ var A08;
     const pad9 = new Audio('samples/snare.mp3');
     const buttonOne = document.getElementById("one");
     buttonOne.addEventListener("click", function () {
-        pad1.play();
+        playSample(new Audio('samples/A.mp3'));
     });
-    function playSample(pad1, pad2, pad3, pad4, pad5, pad6, pad7, pad8, pad9) {
+    const buttonTwo = document.getElementById("two");
+    buttonTwo.addEventListener("click", function () {
+        playSample(new Audio('samples/C.mp3'));
+    });
+    const buttonThree = document.getElementById("three");
+    buttonThree.addEventListener("click", function () {
+        playSample(new Audio('samples/F.mp3'));
+    });
+    const buttonFour = document.getElementById("four");
+    buttonFour.addEventListener("click", function () {
+        playSample(new Audio('samples/G.mp3'));
+    });
+    const buttonFive = document.getElementById("five");
+    buttonFive.addEventListener("click", function () {
+        playSample(new Audio('samples/hihat.mp3'));
+    });
+    const buttonSix = document.getElementById("six");
+    buttonSix.addEventListener("click", function () {
+        playSample(new Audio('samples/kick.mp3'));
+    });
+    const buttonSeven = document.getElementById("seven");
+    buttonSeven.addEventListener("click", function () {
+        playSample(new Audio('samples/laugh-1.mp3'));
+    });
+    const buttonEight = document.getElementById("eight");
+    buttonEight.addEventListener("click", function () {
+        playSample(new Audio('samples/laugh-2.mp3'));
+    });
+    const buttonNine = document.getElementById("nine");
+    buttonNine.addEventListener("click", function () {
+        playSample(new Audio('samples/snare.mp3'));
+    });
+    const buttonPlay = document.getElementById("playbutton");
+    buttonPlay.addEventListener('click', function () {
+        setInterval(function () {
+            playSample(new Audio(arraySamples[ton]));
+            ton++;
+            if (ton == 9) {
+                ton = 0;
+            }
+        });
+    });
+    let ton = 0;
+    let arraySamples = ['samples/A.mp3', 'samples/C.mp3'];
+    function playSample(widergabe) {
+        widergabe.play();
     }
 })(A08 || (A08 = {}));
 //# sourceMappingURL=drums.js.map
--- Abgabe08/drums.ts
+++ Abgabe08/drums.ts
@@ -11,14 +11,65 @@ namespace A08{
     const pad9:HTMLAudioElement = new Audio ('samples/snare.mp3');
 
     const buttonOne: HTMLButtonElement = document.getElementById("one") as HTMLButtonElement;
-
     buttonOne.addEventListener("click", function(){
-        
-        pad1.play()
+        playSample (new Audio ('samples/A.mp3'))
     });
 
-    function playSample(pad1, pad2, pad3, pad4, pad5, pad6, pad7, pad8, pad9){
-        
+    const buttonTwo: HTMLButtonElement = document.getElementById("two") as HTMLButtonElement;
+    buttonTwo.addEventListener("click", function(){
+        playSample (new Audio ('samples/C.mp3'))
+    });
+
+    const buttonThree: HTMLButtonElement = document.getElementById("three") as HTMLButtonElement;
+    buttonThree.addEventListener("click", function(){
+        playSample (new Audio ('samples/F.mp3'))
+    });
+
+    const buttonFour: HTMLButtonElement = document.getElementById("four") as HTMLButtonElement;
+    buttonFour.addEventListener("click", function(){
+        playSample (new Audio ('samples/G.mp3'))
+    });
+
+    const buttonFive: HTMLButtonElement = document.getElementById("five") as HTMLButtonElement;
+    buttonFive.addEventListener("click", function(){
+        playSample (new Audio ('samples/hihat.mp3'))
+    });
+
+    const buttonSix: HTMLButtonElement = document.getElementById("six") as HTMLButtonElement;
+    buttonSix.addEventListener("click", function(){
+        playSample (new Audio ('samples/kick.mp3'))
+    });
+
+    const buttonSeven: HTMLButtonElement = document.getElementById("seven") as HTMLButtonElement;
+    buttonSeven.addEventListener("click", function(){
+        playSample (new Audio ('samples/laugh-1.mp3'))
+    });
+
+    const buttonEight: HTMLButtonElement = document.getElementById("eight") as HTMLButtonElement;
+    buttonEight.addEventListener("click", function(){
+        playSample (new Audio ('samples/laugh-2.mp3'))
+    });
+
+    const buttonNine: HTMLButtonElement = document.getElementById("nine") as HTMLButtonElement;
+    buttonNine.addEventListener("click", function(){
+        playSample (new Audio ('samples/snare.mp3'))
+    });
+
+    const buttonPlay: HTMLButtonElement = document.getElementById("playbutton") as HTMLButtonElement;
+    buttonPlay.addEventListener ('click', function () {
+        setInterval(function () {
+            playSample(new Audio (arraySamples [ton]))
+            ton++
+            if (ton==9) {ton=0}
+        })
+    })
+
+    let ton = 0;
+
+    let arraySamples = ['samples/A.mp3', 'samples/C.mp3']
+
+    function playSample(widergabe: any) {
+        widergabe.play()
     }
 
 }
