e24be4b6e990ee470008c207fedb12912de00846
TimSchneider1 <timleander03@gmail.com>
2022-12-22 15:45:48 +0100

    abgabe10

--- Abgabe10/todolist.js
+++ Abgabe10/todolist.js
@@ -11,16 +11,33 @@ var A10;
     }
     let counter = 0;
     const adder = document.getElementById("taskAddBtn");
-    adder.addEventListener("click", function () {
+    adder.addEventListener('click', function () {
         let taskNew = newTask({ name: input.value, done: false, delete: false });
         console.log(taskNew);
+        const trashCan = document.createElement('div');
+        trashCan.className = "fas fa-trash-alt";
+        const checkBox = document.createElement('div');
+        checkBox.className = "far fa-circle";
+        const checked = document.createElement('div');
+        checked.className = "far fa-times-circle";
         counter++;
         const counting = document.getElementById('currentTasks');
         counting.textContent = String(counter);
         let element = document.createElement('div');
         element.appendChild(new Text(input.value));
+        element.appendChild(trashCan);
+        element.appendChild(checkBox);
         let body = document.getElementById("newTaskAdded");
         body.appendChild(element);
+        trashCan.addEventListener('click', function () {
+            element.remove();
+        });
+        checkBox.addEventListener('click', function () {
+            if (checkBox.classList.contains("fa-circle"))
+                checkBox.classList.replace("fa-circle", "fa-times-circle");
+            else
+                checkBox.classList.replace("fa-times-circle", "fa-circle");
+        });
         const clearLine = document.getElementById("takAddBtn");
         input.value = '';
     });
--- Abgabe10/todolist.ts
+++ Abgabe10/todolist.ts
@@ -22,9 +22,18 @@ namespace A10{
     let counter:number = 0;
 
     const adder: HTMLButtonElement = document.getElementById("taskAddBtn") as HTMLButtonElement;
-    adder.addEventListener("click", function (){
+    adder.addEventListener('click', function (){
         let taskNew = newTask ({name: input.value, done: false, delete: false});
         console.log(taskNew);
+
+        const trashCan = document.createElement('div')
+        trashCan.className = "fas fa-trash-alt"
+
+        const checkBox = document.createElement('div')
+        checkBox.className = "far fa-circle"
+
+        const checked = document.createElement('div')
+        checked.className = "far fa-times-circle"
         
         counter++;
         const counting = document.getElementById('currentTasks') as HTMLElement;
@@ -32,8 +41,23 @@ namespace A10{
         
         let element = document.createElement('div')
         element.appendChild(new Text(input.value))
+        element.appendChild(trashCan);
+        element .appendChild(checkBox)
         let body = document.getElementById("newTaskAdded");
         body.appendChild(element);
+
+        trashCan.addEventListener('click', function () {
+            element.remove()
+        })
+
+        checkBox.addEventListener('click', function () {
+            if( checkBox.classList.contains("fa-circle"))
+            checkBox.classList.replace("fa-circle", "fa-times-circle")
+            else
+            checkBox.classList.replace("fa-times-circle", "fa-circle")
+
+
+        })
 
         const clearLine = document.getElementById("takAddBtn") as HTMLInputElement;
         input.value = '';
